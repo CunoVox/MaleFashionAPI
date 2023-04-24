a5c39436e448a70b39ed5789b8a90f3cc87092da
CunoVox <haovo1512@gmail.com>
2023-04-25 00:21:22 +0700

    first commit

---
+++ src/main/java/StoreApp/StoreApp/service/UserService.java
@@ -0,0 +1,23 @@
+package StoreApp.StoreApp.service;
+
+import java.util.List;
+
+import StoreApp.StoreApp.entity.User;
+
+public interface UserService {
+	List<User> getAllUser();
+
+	User saveUser(User user);
+
+//	User getUserById(String loginname);
+
+	User updateUser(User user);
+
+	void deleteUserById(String id);
+	
+	User GetUserByEmail(String email);
+
+	User findByIdAndRole(String id, String role);
+
+	List<User> findAll();    
+}
